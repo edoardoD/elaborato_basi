ea9fbd327e4634840ebbb2c0a9a9336a54097a28
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-06-13 16:45:16 +0200

    inizializzazione delle tabelle eventi,visite,visitatori, guide, competenze

--- insert.sql
+++ insert.sql
@@ -1,108 +1,161 @@
-INSERT INTO Guide (id_guida, nominativo, sesso, data_nascita, titolo_studio, anno_titolo)
-VALUES (1, 'Mario Rossi', 'M', 1980-01-01, 'Laurea in Storia dell Arte', 2005),
-       (2, 'Giovanna Bianchi', 'F', '1985-02-15', 'Laurea in Archeologia', 2010),
-       (3, 'Marco Verdi', 'M', '1975-05-10', 'Laurea in Lettere', 2000),
-       (4, 'Paola Neri', 'F', 1988-08-20, 'Laurea in Storia dell Arte', 2013),
-       (5, 'Luca Rossi', 'M', 1990-03-15, 'Laurea in Archeologia', 2015),
-       (6, 'Giulia Verdi', 'F', 1982-07-10, 'Laurea in Lettere', 2007),
-       (7, 'Fabio Bianchi', 'M', 1978-11-18, 'Laurea in Storia dell Arte', 2003),
-       (8, 'Alessandra Neri', 'F', '1995-04-25', 'Laurea in Archeologia', 2018),
-       (9, 'Davide Rossi', 'M', '1983-09-30', 'Laurea in Lettere', 2008),
-       (10, 'Federica Verdi', 'F', '1992-12-05', 'Laurea in Storia dell Arte', 2016);
+--guide
+INSERT INTO `guide` (`id_guida`, `nominativo`, `sesso`, `data_nascita`, `titolo_studio`, `anno_titolo`)
+VALUES
+  (1, 'Mario Rossi', 'M', '1980-01-01', 'Laurea in Storia dell''arte', 2005),
+  (2, 'Giovanna Bianchi', 'F', '1990-02-15', 'Diploma Liceo Classico', 2010),
+  (3, 'Marco Verdi', 'M', '1985-05-10', 'Laurea in Storia dell''arte', 2000),
+  (4, 'Paola Neri', 'F', '1998-08-20', 'Diploma Liceo Scientifico', 2013),
+  (5, 'Luca Rossi', 'M', '1995-06-01', 'Laurea in Storia dell''arte', 2015),
+  (6, 'Giulia Verdi', 'F', '1999-09-10', 'Diploma Liceo Classico', 2007),
+  (7, 'Fabio Bianchi', 'M', '1983-07-15', 'Laurea in Storia dell''arte', 2003),
+  (8, 'Alessandra Neri', 'F', '1997-04-25', 'Diploma Liceo Scientifico', 2018),
+  (9, 'Davide Rossi', 'M', '1989-09-30', 'Laurea in Storia dell''arte', 2008),
+  (10, 'Federica Verdi', 'F', '1992-12-05', 'Diploma Liceo Classico', 2016),
+  (11, 'Simone Bianchi', 'M', '1987-03-20', 'Laurea in Storia dell''arte', 2009),
+  (12, 'Laura Neri', 'F', '1993-11-15', 'Diploma Liceo Scientifico', 2014);
 
 
-INSERT INTO Visitatori (id_visitatore, nominativo, nazionalita, telefono, email, lingua_base)
-VALUES (1, 'Giuseppe Bianchi', 'Italiana', '3331234567', 'giuseppe.bianchi@gmail.com', 'Italiano'),
-       (2, 'Maria Rossi', 'Italiana', '3352345678', 'maria.rossi@hotmail.com', 'Italiano'),
-       (3, 'Antonio Verdi', 'Italiana', '3333456789', 'antonio.verdi@gmail.com', 'Italiano'),
-       (4, 'Giovanni Neri', 'Italiana', '3394567890', 'giovanni.neri@gmail.com', 'Italiano'),
-       (5, 'Anna Bianchi', 'Italiana', '3385678901', 'anna.bianchi@hotmail.com', 'Italiano'),
-       (6, 'Lorenzo Rossi', 'Italiana', '3346789012', 'lorenzo.rossi@gmail.com', 'Italiano'),
-       (7, 'Francesca Verdi', 'Italiana', '3397890123', 'francesca.verdi@hotmail.com', 'Italiano'),
-       (8, 'Simone Neri', 'Italiana', '3338901234', 'simone.neri@gmail.com', 'Italiano'),
-       (9, 'Chiara Bianchi', 'Italiana', '3389012345', 'chiara.bianchi@hotmail.com', 'Italiano'),
-       (10, 'Marco Rossi', 'Italiana', '3350123456', 'marco.rossi@gmail.com', 'Italiano');
+-- visitatori
+
+INSERT INTO `visitatori` (`id_visitatore`, `nominativo`, `nazionalita`, `telefono`, `email`, `lingua_base`)
+VALUES
+  (1, 'Giuseppe Bianchi', 'Italiana', '123456789', 'giuseppe@example.com', 'Italiano'),
+  (2, 'Maria Rossi', 'Italiana', '987654321', 'maria@example.com', 'Italiano'),
+  (3, 'John Smith', 'Inglese', '555555555', 'john@example.com', 'Inglese'),
+  (4, 'Emma Johnson', 'Inglese', '444444444', 'emma@example.com', 'Inglese'),
+  (5, 'Hans Müller', 'Tedesca', '111111111', 'hans@example.com', 'Tedesco'),
+  (6, 'Sophie Dupont', 'Francese', '222222222', 'sophie@example.com', 'Francese'),
+  (7, 'Luca Ferrari', 'Italiana', '333333333', 'luca@example.com', 'Italiano'),
+  (8, 'Elena Bianchi', 'Italiana', '999999999', 'elena@example.com', 'Italiano'),
+  (9, 'David Lee', 'Inglese', '777777777', 'david@example.com', 'Inglese'),
+  (10, 'Marta Lopez', 'Spagnola', '666666666', 'marta@example.com', 'Spagnolo'),
+  (11, 'Luisa Moretti', 'Italiana', '+39 123456789', 'luisa@example.com', 'Italiano'),
+  (12, 'Robert Johnson', 'Inglese', '+44 234567890', 'robert@example.com', 'Inglese'),
+  (13, 'Anna Schmidt', 'Tedesca', '+49 345678901', 'anna@example.com', 'Tedesco'),
+  (14, 'Jeanne Dubois', 'Francese', '+33 456789012', 'jeanne@example.com', 'Francese'),
+  (15, 'Mario Bianchi', 'Italiana', '+39 567890123', 'mario@example.com', 'Italiano'),
+  (16, 'Emma Smith', 'Inglese', '+44 678901234', 'emma@example.com', 'Inglese'),
+  (17, 'Hans Müller', 'Tedesca', '+49 789012345', 'hans@example.com', 'Tedesco'),
+  (18, 'Sophie Dupont', 'Francese', '+33 890123456', 'sophie@example.com', 'Francese'),
+  (19, 'Marco Ferrari', 'Italiana', '+39 012345678', 'marco@example.com', 'Italiano'),
+  (20, 'Elena Bianchi', 'Italiana', '+39 234567890', 'elena@example.com', 'Italiano'),
+  (21, 'David Lee', 'Inglese', '+44 345678901', 'david@example.com', 'Inglese'),
+  (22, 'Marta Lopez', 'Spagnola', '+34 456789012', 'marta@example.com', 'Spagnolo'),
+  (23, 'Luigi Russo', 'Italiana', '+39 567890123', 'luigi@example.com', 'Italiano'),
+  (24, 'Emily Wilson', 'Inglese', '+44 678901234', 'emily@example.com', 'Inglese'),
+  (25, 'Thomas Müller', 'Tedesca', '+49 789012345', 'thomas@example.com', 'Tedesco'),
+  (26, 'Camille Martin', 'Francese', '+33 890123456', 'camille@example.com', 'Francese'),
+  (27, 'Giovanni Bianchi', 'Italiana', '+39 012345678', 'giovanni@example.com', 'Italiano'),
+  (28, 'Olivia Johnson', 'Inglese', '+44 234567890', 'olivia@example.com', 'Inglese'),
+  (29, 'Fritz Schmidt', 'Tedesca', '+49 345678901', 'fritz@example.com', 'Tedesco'),
+  (30, 'Amélie Dubois', 'Francese', '+33 456789012', 'amelie@example.com', 'Francese'),
+  (31, 'Alessandro Ferrari', 'Italiana', '+39 567890123', 'alessandro@example.com', 'Italiano'),
+  (32, 'Sophia Bianchi', 'Italiana', '+39 678901234', 'sophia@example.com', 'Italiano'),
+  (33, 'Daniel Johnson', 'Inglese', '+44 789012345', 'daniel@example.com', 'Inglese'),
+  (34, 'Maria Lopez', 'Spagnola', '+34 012345678', 'maria@example.com', 'Spagnolo'),
+  (35, 'Giovanni Rossi', 'Italiana', '+39 901234567', 'giovanni@example.com', 'Italiano'),
+  (36, 'Alice Brown', 'Inglese', '+44 123456789', 'alice@example.com', 'Inglese'),
+  (37, 'Hannah Müller', 'Tedesca', '+49 234567890', 'hannah@example.com', 'Tedesco'),
+  (38, 'Élise Martin', 'Francese', '+33 345678901', 'elise@example.com', 'Francese'),
+  (39, 'Andrea Conti', 'Italiana', '+39 456789012', 'andrea@example.com', 'Italiano'),
+  (40, 'Henry Wilson', 'Inglese', '+44 567890123', 'henry@example.com', 'Inglese'),
+  (41, 'Paul Schmidt', 'Tedesca', '+49 678901234', 'paul@example.com', 'Tedesco'),
+  (42, 'Emma Dubois', 'Francese', '+33 789012345', 'emma@example.com', 'Francese'),
+  (43, 'Francesco Esposito', 'Italiana', '+39 012345678', 'francesco@example.com', 'Italiano'),
+  (44, 'Sophie Johnson', 'Inglese', '+44 234567890', 'sophie@example.com', 'Inglese'),
+  (45, 'Lukas Müller', 'Tedesca', '+49 345678901', 'lukas@example.com', 'Tedesco'),
+  (46, 'Camille Petit', 'Francese', '+33 456789012', 'camille@example.com', 'Francese'),
+  (47, 'Giulia Rizzo', 'Italiana', '+39 567890123', 'giulia@example.com', 'Italiano'),
+  (48, 'Jack Smith', 'Inglese', '+44 678901234', 'jack@example.com', 'Inglese'),
+  (49, 'Julia Wagner', 'Tedesca', '+49 789012345', 'julia@example.com', 'Tedesco'),
+  (50, 'Emma Dupont', 'Francese', '+33 890123456', 'emma@example.com', 'Francese');
 
 
-INSERT INTO Visite (id_visita, denominazione, luogo, durata_media)
-VALUES (1, 'Museo Egizio', 'Torino', 120),
-       (2, 'Museo Nazionale Romano', 'Roma', 180),
-       (3, 'Galleria degli Uffizi', 'Firenze', 150),
-       (4, 'Museo Archeologico Nazionale', 'Napoli', 90),
-       (5, 'Museo del Prado', 'Madrid', 120),
-       (6, 'British Museum', 'Londra', 180),
-       (7, 'Museo del Louvre', 'Parigi', 150),
-       (8, 'Metropolitan Museum of Art', 'New York', 120),
-       (9, 'National Gallery', 'Londra', 90),
-      (10, 'Vatican Museums', 'Città del Vaticano', 180);
+INSERT INTO `competenze` (`id_guida`, `lingua`, `livello`)
+VALUES
+  (1, 'Italiano', 'C2'),
+  (1, 'Inglese', 'C1'),
+  (2, 'Francese', 'B2'),
+  (3, 'Tedesco', 'C1'),
+  (4, 'Inglese', 'B2'),
+  (5, 'Italiano', 'C2'),
+  (6, 'Inglese', 'C1'),
+  (7, 'Francese', 'B2'),
+  (8, 'Tedesco', 'C1'),
+  (9, 'Italiano', 'B2'),
+  (10, 'Inglese', 'C2'),
+  (11, 'Italiano', 'C1'),
+  (12, 'Francese', 'B2'),
+  (2, 'Italiano', 'C1');
 
-INSERT INTO Competenze (id_guida, lingua, livello)
-VALUES (1, 'Italiano', 'Madrelingua'),
-       (1, 'Inglese', 'Avanzato'),
-       (1, 'Francese', 'Intermedio'),
-       (2, 'Italiano', 'Madrelingua'),
-       (2, 'Inglese', 'Avanzato'),
-       (2, 'Spagnolo', 'Intermedio'),
-       (3, 'Italiano', 'Madrelingua'),
-       (3, 'Francese', 'Avanzato'),
-       (3, 'Tedesco', 'Intermedio'),
-       (4, 'Italiano', 'Madrelingua'),
-       (4, 'Inglese', 'Intermedio'),
-       (4, 'Spagnolo', 'Avanzato'),
-       (5, 'Italiano', 'Madrelingua'),
-(5, 'Portoghese', 'Avanzato'),
-(5, 'Cinese', 'Intermedio'),
-(6, 'Italiano', 'Madrelingua'),
-(6, 'Tedesco', 'Avanzato'),
-(6, 'Giapponese', 'Intermedio'),
-(7, 'Italiano', 'Madrelingua'),
-(7, 'Francese', 'Intermedio'),
-(7, 'Arabo', 'Avanzato'),
-(8, 'Italiano', 'Madrelingua'),
-(8, 'Spagnolo', 'Avanzato'),
-(8, 'Russo', 'Intermedio'),
-(9, 'Italiano', 'Madrelingua'),
-(9, 'Inglese', 'Avanzato'),
-(9, 'Olandese', 'Intermedio'),
-(10, 'Italiano', 'Madrelingua'),
-(10, 'Ceco', 'Avanzato'),
-(10, 'Svedese', 'Intermedio');
+-- Inserimento degli eventi per ogni visita
+-- Visita con id_visita = 1
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(1, 1, '2022-06-15', 10.99),
+(2, 1, '2022-06-16', 12.99),
+(3, 1, '2022-06-17', 9.99);
+
+-- Visita con id_visita = 2
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(4, 2, '2022-07-01', 14.99),
+(5, 2, '2022-07-02', 11.99),
+(6, 2, '2022-07-03', 13.99);
+
+-- Visita con id_visita = 3
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(7, 3, '2022-08-10', 9.99),
+(8, 3, '2022-08-11', 12.99),
+(9, 3, '2022-08-12', 10.99);
 
 
-INSERT INTO Eventi (id_evento, id_visita, data, prezzo)
-VALUES (1, 1, '2023-05-01', 15.00),
-       (2, 2, '2023-05-02', 20.00),
-       (3, 3, '2023-05-03', 25.00),
-       (4, 4, '2023-05-04', 10.00),
-       (5, 5, '2023-05-05', 18.00),
-       (6, 6, '2023-05-06', 22.00),
-       (7, 7, '2023-05-07', 30.00),
-       (8, 8, '2023-05-08', 27.00),
-       (9, 9, '2023-05-09', 12.00),
-       (10, 10, '2023-05-10', 35.00);
+-- Visita con id_visita = 4
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(10, 4, '2022-09-05', 11.99),
+(11, 4, '2022-09-06', 9.99),
+(12, 4, '2022-09-07', 12.99);
 
+-- Visita con id_visita = 5
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(13, 5, '2022-10-20', 10.99),
+(14, 5, '2022-10-21', 12.99),
+(15, 5, '2022-10-22', 9.99);
 
-INSERT INTO Gruppi (id_gruppo, id_evento, ora_inizio, id_guida, lingua, min_visitatori, max_visitatori)
-VALUES (1, 1, '10:00:00', 1, 'Italiano', 5, 20),
-       (2, 2, '11:00:00', 2, 'Inglese', 6, 15),
-       (3, 3, '14:00:00', 3, 'Francese', 7, 25),
-       (4, 4, '15:00:00', 4, 'Italiano', 4, 10),
-       (5, 5, '16:00:00', 5, 'Spagnolo', 8, 18),
-       (6, 6, '17:00:00', 6, 'Inglese', 5, 12),
-       (7, 7, '10:30:00', 7, 'Italiano', 10, 30),
-       (8, 8, '11:30:00', 8, 'Italiano', 5, 15),
-       (9, 9, '14:30:00', 9, 'Inglese', 8, 20),
-       (10, 10, '15:30:00', 10, 'Italiano', 15, 40);
+-- Visita con id_visita = 6
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(16, 6, '2022-11-15', 13.99),
+(17, 6, '2022-11-16', 11.99),
+(18, 6, '2022-11-17', 10.99);
 
+-- Visita con id_visita = 7
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(19, 7, '2022-12-03', 9.99),
+(20, 7, '2022-12-04', 12.99),
+(21, 7, '2022-12-05', 10.99);
 
-INSERT INTO Membri (id_gruppo, id_visitatore, tipo_pagamento, descr_pagamento, data_pagamento)
-VALUES (2, 5, 'Bonifico bancario', 'Pagamento effettuato tramite bonifico bancario', '2023-04-15 15:45:00'),
-(1, 2, 'Carta di credito', 'Pagamento effettuato tramite carta di credito Visa', '2023-04-16 10:30:00'),
-(3, 7, 'PayPal', 'Pagamento effettuato tramite PayPal', '2023-04-13 09:30:00'),
-(1, 8, 'Carta di credito', 'Pagamento effettuato tramite carta di credito Mastercard', '2023-04-12 14:20:00'),
-(4, 3, 'Assegno', 'Pagamento effettuato tramite assegno circolare', '2023-04-11 11:30:00'),
-(2, 1, 'Contanti', 'Pagamento effettuato in contanti', '2023-04-10 19:15:00'),
-(3, 6, 'Carta di credito', 'Pagamento effettuato tramite carta di credito Visa', '2023-04-09 12:30:00'),
-(4, 4, 'PayPal', 'Pagamento effettuato tramite PayPal', '2023-04-08 11:45:00'),
-(1, 9, 'Bonifico bancario', 'Pagamento effettuato tramite bonifico bancario', '2023-04-07 14:30:00'),
-(2, 10, 'Assegno', 'Pagamento effettuato tramite assegno bancario', '2023-04-06 17:15:00');
+-- Visita con id_visita = 8
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(22, 8, '2022-12-20', 11.99),
+(23, 8, '2022-12-21', 9.99),
+(24, 8, '2022-12-22', 12.99);
+
+-- Visita con id_visita = 9
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(25, 9, '2022-12-27', 10.99),
+(26, 9, '2022-12-28', 12.99),
+(27, 9, '2022-12-29', 9.99);
+
+-- Visita con id_visita = 10
+INSERT INTO `eventi` (`id_evento`, `id_visita`, `data`, `prezzo`)
+VALUES
+(28, 10, '2022-12-30', 11.99),
+(29, 10, '2022-12-31', 13.99),
+(30, 10, '2023-01-01', 10.99);
