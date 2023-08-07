796a7a5b8a0b7d3408eb6ce6f2aaf726b8e837d2
Edoardo Desiderio <edoardo.desiderio01@gmail.com>
2023-08-07 21:40:40 +0200

    create table database

--- SQL/visite_turistiche.sql
+++ SQL/visite_turistiche.sql
@@ -1,281 +1,162 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Creato il: Giu 13, 2023 alle 15:26
--- Versione del server: 10.4.28-MariaDB
--- Versione PHP: 8.0.28
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-\subsubsection*{Con ridondanza}
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `visite_turistiche`
---
+-- *********************************************
+-- * SQL MySQL generation                      
+-- *--------------------------------------------
+-- * DB-MAIN version: 11.0.2              
+-- * Generator date: Sep 14 2021              
+-- * Generation date: Mon Aug  7 17:00:25 2023 
+-- * LUN file: C:\unibo\elaborato_basi\Db_main\VISITE_TURISTICHE.lun 
+-- * Schema: logico/1-2-2 
+-- ********************************************* 
 
 
---
--- Struttura della tabella `competenze`
---
+-- Database Section
+-- ________________ 
 
-CREATE TABLE `competenze` (
-  `id_guida` int(11) NOT NULL,
-  `lingua` varchar(15) NOT NULL,
-  `livello` varchar(15) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `competenze`
---
-
-INSERT INTO `competenze` (`id_guida`, `lingua`, `livello`) VALUES
-(1, 'Francese', 'Intermedio'),
-(1, 'Inglese', 'Avanzato'),
-(1, 'Italiano', 'Madrelingua'),
-(2, 'Inglese', 'Avanzato'),
-(2, 'Italiano', 'Madrelingua'),
-(2, 'Spagnolo', 'Intermedio'),
-(3, 'Francese', 'Avanzato'),
-(3, 'Italiano', 'Madrelingua'),
-(3, 'Tedesco', 'Intermedio'),
-(4, 'Inglese', 'Intermedio'),
-(4, 'Italiano', 'Madrelingua'),
-(4, 'Spagnolo', 'Avanzato'),
-(5, 'Cinese', 'Intermedio'),
-(5, 'Italiano', 'Madrelingua'),
-(5, 'Portoghese', 'Avanzato'),
-(6, 'Giapponese', 'Intermedio'),
-(6, 'Italiano', 'Madrelingua'),
-(6, 'Tedesco', 'Avanzato'),
-(7, 'Arabo', 'Avanzato'),
-(7, 'Francese', 'Intermedio'),
-(7, 'Italiano', 'Madrelingua'),
-(8, 'Italiano', 'Madrelingua'),
-(8, 'Russo', 'Intermedio'),
-(8, 'Spagnolo', 'Avanzato'),
-(9, 'Inglese', 'Avanzato'),
-(9, 'Italiano', 'Madrelingua'),
-(9, 'Olandese', 'Intermedio'),
-(10, 'Ceco', 'Avanzato'),
-(10, 'Italiano', 'Madrelingua'),
-(10, 'Svedese', 'Intermedio');
+create database TURISMO;
+use TURISMO;
 
 
+-- Tables Section
+-- _____________ 
 
---
--- Struttura della tabella `eventi`
---
+create table BIGLIETTI (
+     nome char(20) not null,
+     comgnome char(20) not null,
+     eta int not null,
+     id int not null,
+     gruppo int not null,
+     ordine -- Compound attribute -- not null,
+     constraint IDBIGLIETTO primary key (id));
 
-CREATE TABLE `eventi` (
-  `id_evento` int(11) NOT NULL,
-  `id_visita` int(11) NOT NULL,
-  `data` date DEFAULT NULL,
-  `prezzo` double DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table CLIENTI (
+     email char(40) not null,
+     nome char(20) not null,
+     cognome char(20) not null,
+     password char(20) not null,
+     nazionalita char(20) not null,
+     telefono int not null UNIQUE,
+     lingua char(20) not null,
+     constraint IDCLIENTE primary key (email));
 
---
--- Dump dei dati per la tabella `eventi`
---
+create table COMPETENZE (
+     lingua char(20) not null,
+     id_guida char(20) not null,
+     livello char(2) not null CHECK (livello in ('B2', 'C1', 'C2'))
+     constraint IDCOMPETENZA primary key (lingua, id_guida));
 
+create table EVENTI (
+     Id int not null AUTO_INCREMENT,
+     prezzo int not null,
+     data DATETIME not null,
+     visita char(1) not null,
+     constraint IDEVENTO primary key (Id));
 
+create table GRUPPI (
+     ora char(1) not null,
+     id char(1) not null,
+     data char(1) not null,
+     max_persone char(1) not null,
+     min_persone char(1) not null,
+     lingua char(1) not null,
+     id_guida char(1) not null,
+     id_evento char(1) not null,
+     constraint IDGRUPPO primary key (id));
 
+create table GUIDE (
+     email char(1) not null,
+     nome char(1) not null,
+     cognome char(1) not null,
+     password char(1) not null,
+     sesso char(1) not null,
+     titolo char(1) not null,
+     anno_titolo char(1) not null,
+     constraint IDGUIDA primary key (email));
 
---
--- Struttura della tabella `gruppi`
---
+create table LINGUE (
+     nome char(20) not null,
+     constraint IDLINGUA primary key (nome));
 
-CREATE TABLE `gruppi` (
-  `id_gruppo` int(11) NOT NULL,
-  `id_evento` int(11) NOT NULL,
-  `ora_inizio` time DEFAULT NULL,
-  `id_guida` int(11) NOT NULL,
-  `lingua` varchar(15) NOT NULL,
-  `min_visitatori` int(11) DEFAULT NULL,
-  `max_visitatori` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+create table METODI (
+     nome char(1) not null,
+     constraint IDMETODO primary key (nome));
 
---
--- Dump dei dati per la tabella `gruppi`
---
+create table ORDINI (
+     prezzo char(1) not null,
+     id -- Compound attribute -- not null,
+     cliente char(1) not null,
+     pagamento char(1) not null,
+     constraint IDORDINE primary key (id -- Compound attribute --));
 
-INSERT INTO `gruppi` (`id_gruppo`, `id_evento`, `ora_inizio`, `id_guida`, `lingua`, `min_visitatori`, `max_visitatori`) VALUES
+create table PAGAMENTI (
+     n_carta char(1) not null,
+     IBAN char(1) not null,
+     id char(1) not null,
+     descrizione  char(1) not null,
+     metodo char(1) not null,
+     constraint IDPAGAMENTO primary key (id));
 
+create table SCONTI (
+     fascia char(1) not null,
+     eta_soglia char(1) not null,
+     percentuale char(1) not null,
+     constraint IDSCONTO primary key (fascia));
 
-
-
---
--- Struttura della tabella `guide`
---
-
-CREATE TABLE `guide` (
-  `id_guida` int(11) NOT NULL,
-  `nominativo` varchar(30) DEFAULT NULL,
-  `sesso` varchar(1) DEFAULT NULL,
-  `data_nascita` date DEFAULT NULL,
-  `titolo_studio` varchar(20) DEFAULT NULL,
-  `anno_titolo` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `guide`
---
-
-INSERT INTO `guide` (`id_guida`, `nominativo`, `sesso`, `data_nascita`, `titolo_studio`, `anno_titolo`) VALUES
-(1, 'Mario Rossi', 'M', '0000-00-00', 'Laurea in Storia del', 2005),
-(2, 'Giovanna Bianchi', 'F', '1985-02-15', 'Laurea in Archeologi', 2010),
-(3, 'Marco Verdi', 'M', '1975-05-10', 'Laurea in Lettere', 2000),
-(4, 'Paola Neri', 'F', '1988-08-20', 'Laurea in Storia del', 2013),
-(5, 'Luca Rossi', 'M', '0000-00-00', 'Laurea in Archeologi', 2015),
-(6, 'Giulia Verdi', 'F', '0000-00-00', 'Laurea in Lettere', 2007),
-(7, 'Fabio Bianchi', 'M', '0000-00-00', 'Laurea in Storia del', 2003),
-(8, 'Alessandra Neri', 'F', '1995-04-25', 'Laurea in Archeologi', 2018),
-(9, 'Davide Rossi', 'M', '1983-09-30', 'Laurea in Lettere', 2008),
-(10, 'Federica Verdi', 'F', '1992-12-05', 'Laurea in Storia del', 2016);
-
-
-
---
--- Struttura della tabella `membri`
---
-
-CREATE TABLE `membri` (
-  `id_gruppo` int(11) NOT NULL,
-  `id_visitatore` int(11) NOT NULL,
-  `tipo_pagamento` varchar(50) DEFAULT NULL,
-  `descr_pagamento` varchar(200) DEFAULT NULL,
-  `data_pagamento` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `membri`
---
-
-
-
---
--- Struttura della tabella `visitatori`
---
-
-CREATE TABLE `visitatori` (
-  `id_visitatore` int(11) NOT NULL,
-  `nominativo` varchar(30) DEFAULT NULL,
-  `nazionalita` varchar(20) DEFAULT NULL,
-  `telefono` varchar(15) DEFAULT NULL,
-  `email` varchar(25) DEFAULT NULL,
-  `lingua_base` varchar(15) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `visitatori`
---
+create table VISITE (
+     nome char(20) not null,
+     descrizione char(100) not null,
+     luogo char(40) not null,
+     durata int not null,
+     constraint IDVISITA primary key (nome));
 
 
+-- Constraints Section
+-- ___________________ 
+
+alter table BIGLIETTI add constraint FKINGRESSO
+     foreign key (gruppo)
+     references GRUPPI (id);
+
+alter table BIGLIETTI add constraint FKVALIDAZIONE
+     foreign key (ordine -- Compound attribute --)
+     references ORDINI (id -- Compound attribute --);
+
+alter table CLIENTI add constraint FKPREFERENZA
+     foreign key (lingua)
+     references LINGUE (nome);
+
+alter table COMPETENZE add constraint FKSTUDIO
+     foreign key (lingua)
+     references LINGUE (nome);
+
+alter table COMPETENZE add constraint FKPOSSESSO
+     foreign key (id_guida)
+     references GUIDE (email);
+
+alter table EVENTI add constraint FKORGANIZAZIONE
+     foreign key (visita)
+     references VISITE (nome);
+
+alter table GRUPPI add constraint FKDIREZIONE
+     foreign key (lingua, id_guida)
+     references COMPETENZE (lingua, id_guida);
+
+alter table GRUPPI add constraint FKPARTECIPAZIONE
+     foreign key (id_evento)
+     references EVENTI (Id);
+
+alter table ORDINI add constraint FKEFFETTUAZIONE
+     foreign key (cliente)
+     references CLIENTI (email);
+
+alter table ORDINI add constraint FKCREDITO
+     foreign key (pagamento)
+     references PAGAMENTI (id);
+
+alter table PAGAMENTI add constraint FKUTILIZZO
+     foreign key (metodo)
+     references METODI (nome);
 
 
---
--- Struttura della tabella `visite`
---
+-- Index Section
+-- _____________ 
 
-CREATE TABLE `visite` (
-  `id_visita` int(11) NOT NULL,
-  `denominazione` varchar(40) DEFAULT NULL,
-  `luogo` varchar(25) DEFAULT NULL,
-  `durata_media` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `visite`
---
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `competenze`
---
-ALTER TABLE `competenze`
-  ADD PRIMARY KEY (`id_guida`,`lingua`);
-
---
--- Indici per le tabelle `eventi`
---
-ALTER TABLE `eventi`
-  ADD PRIMARY KEY (`id_evento`),
-  ADD KEY `VisiteEventi` (`id_visita`);
-
---
--- Indici per le tabelle `gruppi`
---
-ALTER TABLE `gruppi`
-  ADD PRIMARY KEY (`id_gruppo`),
-  ADD KEY `CompetenzeGruppi` (`id_guida`,`lingua`),
-  ADD KEY `EventiGruppi` (`id_evento`);
-
---
--- Indici per le tabelle `guide`
---
-ALTER TABLE `guide`
-  ADD PRIMARY KEY (`id_guida`);
-
---
--- Indici per le tabelle `membri`
---
-ALTER TABLE `membri`
-  ADD PRIMARY KEY (`id_gruppo`,`id_visitatore`),
-  ADD KEY `VisitatoriMembri` (`id_visitatore`);
-
---
--- Indici per le tabelle `visitatori`
---
-ALTER TABLE `visitatori`
-  ADD PRIMARY KEY (`id_visitatore`);
-
---
--- Indici per le tabelle `visite`
---
-ALTER TABLE `visite`
-  ADD PRIMARY KEY (`id_visita`);
-
---
--- Limiti per le tabelle scaricate
---
-
---
--- Limiti per la tabella `competenze`
---
-ALTER TABLE `competenze`
-  ADD CONSTRAINT `GuideCompetenze` FOREIGN KEY (`id_guida`) REFERENCES `guide` (`id_guida`);
-
---
--- Limiti per la tabella `eventi`
---
-ALTER TABLE `eventi`
-  ADD CONSTRAINT `VisiteEventi` FOREIGN KEY (`id_visita`) REFERENCES `visite` (`id_visita`);
-
---
--- Limiti per la tabella `gruppi`
---
-ALTER TABLE `gruppi`
-  ADD CONSTRAINT `CompetenzeGruppi` FOREIGN KEY (`id_guida`,`lingua`) REFERENCES `competenze` (`id_guida`, `lingua`),
-  ADD CONSTRAINT `EventiGruppi` FOREIGN KEY (`id_evento`) REFERENCES `eventi` (`id_evento`);
-
---
--- Limiti per la tabella `membri`
---
-ALTER TABLE `membri`
-  ADD CONSTRAINT `GruppiMembri` FOREIGN KEY (`id_gruppo`) REFERENCES `gruppi` (`id_gruppo`),
-  ADD CONSTRAINT `VisitatoriMembri` FOREIGN KEY (`id_visitatore`) REFERENCES `visitatori` (`id_visitatore`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
